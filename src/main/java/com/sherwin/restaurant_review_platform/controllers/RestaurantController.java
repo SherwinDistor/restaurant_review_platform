1017b23cdd2f1ba1f0a4ff100dc48f36a5996ea5
sherwindistor <182566354+SherwinDistor@users.noreply.github.com>
2025-11-25 13:59:22 -0600

    Initial commit

---
+++ src/main/java/com/sherwin/restaurant_review_platform/controllers/RestaurantController.java
@@ -0,0 +1,48 @@
+package com.sherwin.restaurant_review_platform.controllers;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sherwin.restaurant_review_platform.domain.dtos.CreateRestaurantDto;
+import com.sherwin.restaurant_review_platform.domain.dtos.RestaurantDto;
+import com.sherwin.restaurant_review_platform.services.RestaurantService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/restaurant")
+@RequiredArgsConstructor
+public class RestaurantController {
+
+    private final RestaurantService restaurantService;
+
+    @GetMapping
+    public ResponseEntity<List<RestaurantDto>> getAllRestaurants() {
+        List<RestaurantDto> allRestaurants = restaurantService.getAllRestaurants();
+
+        return new ResponseEntity<>(allRestaurants, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<RestaurantDto> createRestaurant(@RequestBody CreateRestaurantDto createRestaurantDto) {
+        RestaurantDto createdRestaurant = restaurantService.createRestaurant(createRestaurantDto);
+        return new ResponseEntity<>(createdRestaurant, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RestaurantDto> getRestaurantById(@PathVariable Integer id) {
+        RestaurantDto restaurant = restaurantService.getRestaurantById(id);
+
+        return new ResponseEntity<>(restaurant, HttpStatus.OK);
+    }
+
+}
