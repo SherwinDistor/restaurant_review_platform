bddb8377e75bcee013b4b1ff161a1b9a518cc4e3
sherwindistor <182566354+SherwinDistor@users.noreply.github.com>
2025-12-17 18:04:00 -0600

    Add security filter chain and allow get methods to the restaurant endpoint

---
+++ src/main/java/com/sherwin/restaurant_review_platform/config/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.sherwin.restaurant_review_platform.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers(HttpMethod.GET, "/api/v1/restaurant/**").permitAll()
+            .anyRequest().authenticated());
+
+    return http.build();
+  }
+
+}
