2079a071fc162d0bdfca2d2e297bf8741b290cf6
sherwindistor <182566354+SherwinDistor@users.noreply.github.com>
2025-12-22 11:39:23 -0600

    Set up command line runner to create an admin user on start up

--- src/main/java/com/sherwin/restaurant_review_platform/RestaurantReviewPlatformApplication.java
+++ src/main/java/com/sherwin/restaurant_review_platform/RestaurantReviewPlatformApplication.java
@@ -1,7 +1,18 @@
 package com.sherwin.restaurant_review_platform;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.sherwin.restaurant_review_platform.domain.entities.RestaurantUser;
+import com.sherwin.restaurant_review_platform.domain.entities.Role;
+import com.sherwin.restaurant_review_platform.repositories.RestaurantUserRepository;
+import com.sherwin.restaurant_review_platform.repositories.RoleRepository;
 
 @SpringBootApplication
 public class RestaurantReviewPlatformApplication {
@@ -10,4 +21,34 @@ public class RestaurantReviewPlatformApplication {
 		SpringApplication.run(RestaurantReviewPlatformApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner run(RoleRepository roleRepository,
+			RestaurantUserRepository restaurantUserRepository,
+			PasswordEncoder passwordEncoder) {
+
+		return args -> {
+			if (roleRepository.findByAuthority("ADMIN").isPresent()) {
+				return;
+			}
+
+			Role admin = new Role();
+			admin.setAuthority("ADMIN");
+			Role saveAdmin = roleRepository.save(admin);
+
+			Role user = new Role();
+			user.setAuthority("USER");
+			roleRepository.save(user);
+
+			Set<Role> roles = new HashSet<>();
+			roles.add(saveAdmin);
+
+			RestaurantUser restaurantUserAdmin = new RestaurantUser();
+			restaurantUserAdmin.setUsername("admin");
+			restaurantUserAdmin.setPassword(passwordEncoder.encode("password"));
+			restaurantUserAdmin.setAuthorities(roles);
+
+			restaurantUserRepository.save(restaurantUserAdmin);
+
+		};
+	}
 }
